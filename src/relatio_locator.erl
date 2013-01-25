c14d9467395bea0ededaaee033db4fbc62de87d5
Uvarov Michael <freeakk@gmail.com>
2013-01-25 23:29:41 +0400

    Add relatio.config.

--- src/relatio_locator.erl
+++ src/relatio_locator.erl
@@ -2,54 +2,39 @@
 -export([locate_world_xref_server/0, locate_world_inferno_server/0]).
 
 locate_world_xref_server() ->
-    %% TODO: rewrite this function completely.
-    Dir1 = "/home/user/erlang/esl/ejabberd/apps/ejabberd",
-    Dir2 = code:lib_dir(stdlib),
-    Dir3 = code:lib_dir(kernel),
-    Dir4 = code:lib_dir(mnesia),
-    Dir5 = code:lib_dir(snmp),
     case whereis(relatio_world) of
         undefined ->
             {ok, Xref} = xref:start(relatio_world),
-            xref:add_application(Xref, Dir1),
-            xref:add_application(Xref, Dir2),
-            xref:add_application(Xref, Dir3),
-            xref:add_application(Xref, Dir4),
-            xref:add_application(Xref, Dir5),
+            link(Xref),
+            Info = locate_world_inferno_server(),
+            App2Dirs = inferno_server:application_directories(Info),
+            io:format(user, "App2Dirs: ~p~n", [App2Dirs]),
+            [xref:add_application(Xref, Dir, [{name, AppName}])
+             || {AppName, [Dir|_]} <- App2Dirs],
+            unlink(Xref),
             Xref;
         Xref ->
             Xref
     end.
 
 locate_world_inferno_server() ->
-    %% TODO: rewrite this function completely.
-    Dir1 = "/home/user/erlang/esl/ejabberd/apps/ejabberd",
-    Dir2 = code:lib_dir(stdlib),
-    Dir3 = code:lib_dir(kernel),
-    Dir4 = code:lib_dir(mnesia),
-    Dir5 = code:lib_dir(snmp),
-    Dir2s = "/home/user/erlang/otp/lib/stdlib",
-    Dir3s = "/home/user/erlang/otp/lib/kernel",
-    Dir4s = "/home/user/erlang/otp/lib/mnesia",
-    Dir5s = "/home/user/erlang/otp/lib/snmp",
-
     case whereis(inferno_server) of
         undefined ->
             {ok, Info} = inferno_server:start([]),
-            inferno_server:add_application(Info, ejabberd, Dir1),
-
-            inferno_server:add_application(Info, stdlib,   Dir2s),
-            inferno_server:add_application(Info, kernel,   Dir3s),
-            inferno_server:add_application(Info, mnesia,   Dir4s),
-            inferno_server:add_application(Info, snmp,     Dir5s),
-
-            inferno_server:add_application(Info, stdlib,   Dir2),
-            inferno_server:add_application(Info, kernel,   Dir3),
-            inferno_server:add_application(Info, mnesia,   Dir4),
-            inferno_server:add_application(Info, snmp,     Dir5),
+            link(Info),
+            {ok, B} = file:read_file("relatio.config"),
+            S = binary_to_list(B),
+            {ok, Tokens, _} = erl_scan:string(S),
+            {ok, [Form]} = erl_parse:parse_exprs(Tokens),
+            {value, Value, _} = erl_eval:expr(Form, []),
+            io:format(user, "Config: ~p~n", [Value]),
+            [handle_config_command(X, Info) || X <- Value],
             erlang:register(inferno_server, Info),
+            unlink(Info),
             Info;
         Info ->
             Info
     end.
 
+handle_config_command({application, Name, Dir}, Info) ->
+    inferno_server:add_application(Info, Name, Dir).
